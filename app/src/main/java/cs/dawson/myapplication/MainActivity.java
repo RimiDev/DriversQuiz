20a556382fdbed862d25f42439b2422fdfd988ad
Nicolas Mazzone <n.mazzone@me.com>
2017-09-29 15:03:15 -0400

    added lifecycle methods

--- app/src/main/java/cs/dawson/myapplication/MainActivity.java
+++ app/src/main/java/cs/dawson/myapplication/MainActivity.java
@@ -1,13 +1,54 @@
 package cs.dawson.myapplication;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    int quizAttempts, tries, correct, incorrect;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        quizAttempts = prefs.getInt("QuizAttempts", 0);
+        tries = prefs.getInt("QuizAttempts", 0);
+        correct = prefs.getInt("QuizAttempts", 0);
+        incorrect = prefs.getInt("QuizAttempts", 0);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
     }
 }
